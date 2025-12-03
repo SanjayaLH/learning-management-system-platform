db88dbe67a09b31399cfd1297746fd8c72b44e4a
SanjayaLH <sanjayahalwala@gmail.com>
2025-12-03 14:15:18 +0530

    Initial commit - Learning Management System Platform project,ER-diagram v1

---
+++ src/test/java/com/sanjaya/lms_platform/LmsPlatformApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sanjaya.lms_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class LmsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
